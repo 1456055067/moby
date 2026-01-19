4fe73a317c3af63d3c31c1cdee348e8c8e6e9a3e
John Edward Willman V <jwillman@jhics.org>
2026-01-19 15:51:08 -0600

    chore: added ipv6 local and global pool

--- daemon/libnetwork/ipamutils/utils.go
+++ daemon/libnetwork/ipamutils/utils.go
@@ -19,6 +19,16 @@ var (
 	globalScopeDefaultNetworks = []*NetworkToSplit{
 		{netip.MustParsePrefix("10.0.0.0/8"), 24},
 	}
+
+	// IPv6 Unique Local Addresses (ULA)(RFC 4193) for local scope networks.
+	localScopeDefaultNetworksV6 = []*NetworkToSplit{
+		{netip.MustParsePrefix("fd00::/48"), 64}, // ~65k /64 subnets, each with over 18 quintillion addresses
+	}
+
+	// IPv6 Unique Local Addresses for global scope networks.
+	globalScopeDefaultNetworksV6 = []*NetworkToSplit{
+		{netip.MustParsePrefix("fd00:0:1::/48"), 64},
+	}
 )
 
 // NetworkToSplit represent a network that has to be split in chunks with mask length Size.
@@ -49,4 +59,14 @@ func GetGlobalScopeDefaultNetworks() []*NetworkToSplit {
 // GetLocalScopeDefaultNetworks returns a copy of the default local-scope network list.
 func GetLocalScopeDefaultNetworks() []*NetworkToSplit {
 	return slices.Clone(localScopeDefaultNetworks)
+}
+
+// GetLocalScopeDefaultNetworksV6 returns a copy of the default local-scope IPv6 network list.
+func GetLocalScopeDefaultNetworksV6() []*NetworkToSplit {
+	return slices.Clone(localScopeDefaultNetworksV6)
+}
+
+// GetGlobalScopeDefaultNetworksV6 returns a copy of the global-scope IPv6 network list.
+func GetGlobalScopeDefaultNetworksV6() []*NetworkToSplit {
+	return slices.Clone(globalScopeDefaultNetworksV6)
 }
